2048bf0aa81cf2305b2b07e0afd72f3de9114822
Javad <jalizade3d@yahoo.com>
2018-07-31 19:09:13 -0700

    Book completed

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,30 @@
+import java.io.Serializable;
 
-public class Book {
+public class Book implements Serializable{
+	private static final long serialVersionUID = -5431267952765863338L;
+	private int isbn;
+	private String title, author;
+	private double price;
+	
+	public Book() {
+		this.isbn = 0;
+		this.title = null;
+		this.author = null;
+		this.price = 0;
+	}
+	
+	public Book(int isbn, String title, String author, double price) {
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
 
+	@Override
+	public String toString() {
+		return "\nBook: \nisbn=" + isbn + "\ntitle=" + title + 
+				"\nauthor=" + author + "\nprice=" + price ;
+	}
+	
+	
 }
